d7bf775737ed06b90894cd150d40cc21d07221d5
JankieQwQ <qwq@awaish.link>
2024-05-18 18:27:18 +0800

    feat: Update setup.py

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wspry',
+    version = '0.1.4',
+    keywords='spring web',
+    description = 'Spring For Python',
+    license = 'Apache License 2.0',
+    url = 'https://github.com/huanyu-neo/Spry',
+    author = 'Huanyu Inc.',
+    author_email = 'admin@duowan.click',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
